b05ba437d91dce45fd137361ba96d64877ab8a0a
plumthedev <kacper.pruszynski@compania.com.pl>
2019-10-17 15:12:48 +0200

    add wandering button project

---
+++ wandering-button/wandering-button.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WanderingButton
+{
+    partial class WanderingButtonForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wanderingButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // wanderingButton
+            // 
+            this.wanderingButton.Location = new System.Drawing.Point(12, 12);
+            this.wanderingButton.Name = "wanderingButton";
+            this.wanderingButton.Size = new System.Drawing.Size(75, 23);
+            this.wanderingButton.TabIndex = 0;
+            this.wanderingButton.Text = "Wędruj";
+            this.wanderingButton.UseVisualStyleBackColor = true;
+            // 
+            // WanderingButtonForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.wanderingButton);
+            this.Name = "WanderingButtonForm";
+            this.Text = "WanderingButtonForm";
+            this.Load += new System.EventHandler(this.WanderingButtonForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button wanderingButton;
+    }
+}
+
---
+++ wandering-button/wandering-button.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WanderingButton
+{
+    public partial class WanderingButtonForm : Form
+    {
+        public WanderingButtonForm()
+        {
+            InitializeComponent();
+        }
+
+        private void WanderingButtonForm_Load(object sender, EventArgs e)
+        {
+            PersonalizeFormWindow();
+            AddButtonEventListners();
+        }
+
+        public void PersonalizeFormWindow()
+        {
+            Text = "Zadanie - Wandering Button";
+            MaximizeBox = false;
+        }
+
+        public void AddButtonEventListners() {
+            wanderingButton.Click += new EventHandler(WanderButtonHandler);
+        }
+
+        public void WanderButtonHandler(object sender, EventArgs e) {
+            int newButtonX = GenerateRandomNumber(0, Width / 2);
+            int newButtonY = GenerateRandomNumber(0, Height / 2);
+            SetButtonPosition(newButtonX, newButtonY);
+        }
+
+        private void SetButtonPosition(int x, int y) {
+            Point buttonLocationPoint = new Point(x, y);
+            wanderingButton.Location = buttonLocationPoint;
+        }
+
+        private int GenerateRandomNumber(int min, int max) {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+    }
+}
