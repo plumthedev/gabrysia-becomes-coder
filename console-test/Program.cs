f91310a0757c7273ee61c9aff637a968b63fe03f
plumthedev <kacper.pruszynski@compania.com.pl>
2019-10-17 17:46:12 +0200

    refactor console-test

--- console-test/Program.cs
+++ console-test/Program.cs
@@ -1,57 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleTest
+namespace console_test
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Wciśnij dowolny klawisz aby ustawić kolor tła konsoli");
-			Console.ReadKey(true);
-			Console.BackgroundColor = ConsoleColor.Magenta;
-			Console.Clear();
-			Console.WriteLine("Kolor tła konsoli został ustawiony");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wciśnij dowolny klawisz aby ustawić kolor tła konsoli");
+            Console.ReadKey(true);
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.Clear();
+            Console.WriteLine("Kolor tła konsoli został ustawiony");
 
-			Console.WriteLine("Wciśnij dowolny klawisz aby ustawić kolor tła tekstu");
-			Console.ReadKey(true);
-			Console.BackgroundColor = ConsoleColor.Cyan;
-			Console.WriteLine("Kolor tekstu został ustawiony");
+            Console.WriteLine("Wciśnij dowolny klawisz aby ustawić kolor tła tekstu");
+            Console.ReadKey(true);
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Kolor tekstu został ustawiony");
 
-			try
-			{
-				Console.WriteLine("Wciśnij dowolny klawisz aby zmienić rozmiar okna");
-				Console.ReadKey(true);
-				Console.SetWindowSize(600, 800);
-				Console.WriteLine("Zmieniono rozmiar okna");
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine("Nie udało się zmienić rozmiaru okna z powodu ograniczeń systemowych");
-				Console.WriteLine(e.Message);
-			}
+            try {
+                Console.WriteLine("Wciśnij dowolny klawisz aby zmienić rozmiar okna");
+                Console.ReadKey(true);
+                Console.SetWindowSize(50, 50);
+                Console.WriteLine("Zmieniono rozmiar okna");
+            } catch (Exception e) {
+                Console.WriteLine("Nie udało się zmienić rozmiaru okna z powodu ograniczeń systemowych");
+                Console.WriteLine(e.Message);
+            }
 
-			Console.WriteLine("Wciśnij dowolny klawisz aby uruchomić dzwięk ");
-			Console.ReadKey(true);
-			Console.Beep();
-			Console.WriteLine("Uruchomiono dzwięk");
+            Console.WriteLine("Wciśnij dowolny klawisz aby uruchomić dzwięk ");
+            Console.ReadKey(true);
+            Console.Beep();
+            Console.WriteLine("Uruchomiono dzwięk");
 
-			Console.WriteLine("Wciśnij dowolny klawisz aby wyczyścić konsole");
-			Console.ReadKey(true);
-			Console.Clear();
-			Console.WriteLine("Wyczyszczono konsole");
+            Console.WriteLine("Wciśnij dowolny klawisz aby wyczyścić konsole");
+            Console.ReadKey(true);
+            Console.Clear();
+            Console.WriteLine("Wyczyszczono konsole");
 
-			try
-			{
-				Console.WriteLine("Wciśnij dowolny klawisz aby ustawić wielkość kursora");
-				Console.ReadKey(true);
-				Console.CursorSize = 25;
-				Console.WriteLine("Ustawiono wielkość kursora");
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine("Nie udało się zmienić rozmiaru kursora z powodu ograniczeń systemowych");
-				Console.WriteLine(e.Message);
-			}
-		}
-	}
+            try {
+                Console.WriteLine("Wciśnij dowolny klawisz aby ustawić wielkość kursora");
+                Console.ReadKey(true);
+                Console.CursorSize = 75;
+                Console.WriteLine("Ustawiono wielkość kursora");
+            } catch (Exception e) {
+                Console.WriteLine("Nie udało się zmienić rozmiaru kursora z powodu ograniczeń systemowych");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey(true);
+        }
+    }
 }
