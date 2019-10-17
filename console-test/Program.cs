1d34f64b39dfdf6907f7cb0cfbb35dee281aa7fd
plumthedev <kacper.pruszynski@compania.com.pl>
2019-10-17 17:48:15 +0200

    fix typo

--- console-test/Program.cs
+++ console-test/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace console_test
+namespace ConsoleTest
 {
     class Program
     {
