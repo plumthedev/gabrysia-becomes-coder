59396c84d39bdb9aa2988d63f2337917290b1c48
plumthedev <kacper.pruszynski@compania.com.pl>
2019-11-15 15:20:20 +0100

    add return indivisible exercise

---
+++ return-indivisible/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ReturnIndivisible
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n = 30;
+            Console.WriteLine(ZwrocNiepodzielne(n)); ;
+            Console.ReadKey();
+        }
+
+        static string ZwrocNiepodzielne(uint n)
+        {
+            bool greaterThanZero = (n > 0);
+            bool dividedByThree = (n % 3 == 0);
+            bool dividedByFive = (n % 5 == 0);
+
+            if (greaterThanZero)
+            {
+                if (!dividedByThree && !dividedByFive)
+                {
+                    string spacing = (n - 1 > 0) ? " " : "";
+                    return ZwrocNiepodzielne(n - 1) + spacing + n;
+                }
+
+                return ZwrocNiepodzielne(n - 1);
+            }
+
+            return "";
+        }
+    }
+}
---
+++ return-indivisible/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("return-indivisible")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("return-indivisible")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("b9da66c2-ced1-4d74-9015-72100ce1b0df")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
